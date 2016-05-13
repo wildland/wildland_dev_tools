5d1fd8628fb02802788215d0602b1d54c411aae8
Joe Weakley <joew@samjoe.com>
2016-05-13 13:09:08 -0700

    Deploy to staging command.

--- lib/wildland_dev_tools.rb
+++ lib/wildland_dev_tools.rb
@@ -5,4 +5,6 @@ module WildlandDevTools
   require 'wildland_dev_tools/railtie' if defined?(Rails)
   require 'wildland_dev_tools/updater'
   require 'wildland_dev_tools/heroku'
+
+  class GitSyncException < StandardError; end
 end
